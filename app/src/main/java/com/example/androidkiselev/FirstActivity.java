f00c4a074965de81c0bdec9a043ab395895c0cee
vanekKiselev <66440054+vanekKiselev@users.noreply.github.com>
2020-12-19 00:33:18 +0300

    First commit

---
+++ app/src/main/java/com/example/androidkiselev/FirstActivity.java
@@ -0,0 +1,15 @@
+package com.example.androidkiselev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        setTitle("FirstActivity");
+    }
+}
